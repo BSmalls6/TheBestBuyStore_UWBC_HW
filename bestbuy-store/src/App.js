97e327e2439d5d5f1f0ff4b718947aeea72f69bf
BSmalls6 <smallwoodbrennan@gmail.com>
2020-02-13 18:58:03 -0800

    getting started

---
+++ bestbuy-store/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { Button, Card, Row, Col } from 'react-materialize';
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
+
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+             </div>
+       </BrowserRouter>
+      
+    );
+  }
+}
+
+export default App;
